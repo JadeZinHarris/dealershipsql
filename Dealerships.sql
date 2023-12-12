00cc2fe5fb36d9f57d21ae0ef1b343b614253a1d
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-12-11 23:24:56 -0500

    Fixed the error with the sql file

--- Dealerships.sql
+++ Dealerships.sql
@@ -14,10 +14,10 @@ USE dealerships;
 #------------------------------------------------------------------#
 
 CREATE TABLE `dealerships` (
-`Dealership_id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`Dealership` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `Name` VARCHAR (50) NOT NULL,
 `Address` VARCHAR(50) NOT NULL,
-`Phone` VARCHAR (12) NOT NULL
+`Phone` VARCHAR (12) 
 );
 
 #------------------------------------------------------------------#
@@ -28,9 +28,10 @@ CREATE TABLE `dealerships` (
 CREATE TABLE `vehicles` (
 `Make` VARCHAR(30) NOT NULL,
 `Model` VARCHAR(30) NOT NULL,
-`Color` VARCHAR (30) NOT NULL,
-`VIN` VARCHAR (30)  NOT NULL,
-`Sold` VARCHAR (3) NOT NULL,
+`Color` VARCHAR (30),
+`VIN` DOUBLE NOT NULL,
+`Sold` BOOLEAN ,
+`Price` DOUBLE,
 CONSTRAINT `PK_vehicles` PRIMARY KEY (`VIN`)
 );
 
@@ -41,15 +42,10 @@ CONSTRAINT `PK_vehicles` PRIMARY KEY (`VIN`)
 
 CREATE TABLE `inventory` (
 `dealership_id` INT,
-`VIN` VARCHAR(30)
+`VIN` DOUBLE NOT NULL,
+FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`),
+FOREIGN KEY (`dealership_id`) REFERENCES `dealerships` (`Dealership`)
 );
-
-ALTER TABLE `inventory` ADD CONSTRAINT `FK_inventory_inventorys` 
-    FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`);
-    
-
-ALTER TABLE `inventory` ADD CONSTRAINT `FK_inventory_inventory` 
-    FOREIGN KEY (`dealership_id`) REFERENCES `dealerships` (`Dealership_id`);
     
     
     
@@ -62,12 +58,13 @@ ALTER TABLE `inventory` ADD CONSTRAINT `FK_inventory_inventory`
     
     
 CREATE TABLE `sales_contract` (
-`Salescontract_id` INT NOT NULL AUTO_INCREMENT,
-`VIN` VARCHAR(30)
+`Salescontract` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`VIN` DOUBLE NOT NULL,
+`Salesprice` DOUBLE NOT NULL,
+`SaleDate` DATE DEFAULT(CURRENT_DATE),
+FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`)
 );
     
-ALTER TABLE `sales_contract` ADD CONSTRAINT `FK_salescontract_inventory` 
-FOREIGN KEY (`VIN`) REFERENCES `vehicles` (`VIN`);
 
 
 #------------------------------------------------------------------#
@@ -78,21 +75,23 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE dealerships;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO dealerships (Dealership_id, Name, Address, Phone)
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
 VALUES(1, 'Carleston','310 w brandon st','111-111-1111');
 
-INSERT INTO dealerships (Dealership_id, Name, Address, Phone)
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
 VALUES(2, 'Greendale','255 n crandberry ave','222-222-2222');
 
-INSERT INTO dealerships (Dealership_id, Name, Address, Phone)
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
 VALUES(3, 'Avery','120 S avery st','333-333-3333');
 
-INSERT INTO dealerships (Dealership_id, Name, Address, Phone)
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
 VALUES(4, 'Can','602 Pen drive','444-444-4444');
 
-INSERT INTO dealerships (Dealership_id, Name, Address, Phone)
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
 VALUES(5, 'Pickles','4235 n apples ave','223-222-2322');
 
+INSERT INTO dealerships (Dealership, Name, Address, Phone)
+VALUES(6, 'Grips','35 ples ave','223-343-2322');
 
 
 #------------------------------------------------------------------#
@@ -105,23 +104,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE vehicles;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Nissan', 'Altima','Green','2345646464646646545','Yes');
+INSERT INTO vehicles (Make, Model, Color, VIN, Sold, Price)
+VALUES('Nissan', 'Altima','Green','234343444445',True, 16000);
 
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Ford', 'Branco','Blue','4541515151345123','No');
+INSERT INTO vehicles (Make, Model, Color, VIN, Sold, Price)
+VALUES('Ford', 'Branco','Blue','45415155123',False, 23000);
 
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Nissan', 'Pathfinder','Black','21515432561664316161616','Yes');
+INSERT INTO vehicles (Make, Model, Color, VIN, Sold, Price)
+VALUES('Nissan', 'Pathfinder','Black','2664316161616',True, 43000);
 
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Toyota', 'Camery','Silver','16626265463616203','No');
+INSERT INTO vehicles (Make, Model, Color, VIN, Sold, Price)
+VALUES('Toyota', 'Camery','Silver','1665526265203',False, 23000);
 
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Honda', 'Civic','Red','654526421616161616416136523','Yes');
-
-INSERT INTO vehicles (Make, Model, Color, VIN, Sold)
-VALUES('Nissan', 'Altima','Green','2345646464646646545','Yes');
+INSERT INTO vehicles (Make, Model, Color, VIN, Sold, Price)
+VALUES('Honda', 'Civic','Red','6545416136523',True, 81000);
 
 
 #------------------------------------------------------------------#
@@ -158,13 +154,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE sales_contract;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO sales_contract (Salescontract_id, VIN)
-VALUES(1, '21515432561664316161616');
+INSERT INTO sales_contract (Salescontract, VIN, SalesPrice, Saledate)
+VALUES(1, '21515432561664316161616',40000, '2023-07-02');
 
-INSERT INTO sales_contract (Salescontract_id, VIN)
-VALUES(3, '654526421616161616416136523');
+INSERT INTO sales_contract (Salescontract, VIN,SalesPrice,Saledate)
+VALUES(3, '654526421616161616416136523',30000, '2023-02-03');
 
-INSERT INTO sales_contract (Salescontract_id, VIN)
-VALUES(2, '2345646464646646545');
+INSERT INTO sales_contract (Salescontract, VIN, SalesPrice,Saledate)
+VALUES(2, '2345646464646646545',12000,'2023-08-26');
 
 
